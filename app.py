6ef2157f12d1fec4c86e424e1e4e60510d70640e
Hassan El Khatib <hmkhatib15@gmail.com>
2022-03-31 01:21:14 +0300

    integrated frontend of login, signup, and news page

--- app.py
+++ app.py
@@ -1,6 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
-@app.route('/', methods=['GET'])
-def func():
-    return "test"
+
+
+@app.route('/')
+def main():
+    return redirect(url_for('getLogin'))
+
+
+@app.route('/login', methods=['GET'])
+def getLogin():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def postLogin():
+    # logging in code goes here
+    return render_template('login.html')
+
+
+@app.route('/signup', methods=['GET'])
+def getSignup():
+    return render_template('signup.html')
+
+
+@app.route('/signup', methods=['GET'])
+def postSignup():
+    # signing up code goes here
+    return render_template('signup.html')
+
+
+@app.route('/news', methods=['GET'])
+def getNews():
+    return render_template('news.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
