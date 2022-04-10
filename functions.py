aef234f3ba0e07070b014d7ae0a5b53e5a15d10b
Hassan El Khatib <hmkhatib15@gmail.com>
2022-04-10 23:11:47 +0300

    integrated sqlite db to backend

--- functions.py
+++ functions.py
@@ -1,35 +1,33 @@
 import mysql.connector
+import sqlite3
 
 
 def authenticate(username, password):
-    conn = mysql.connector.connect(host='35.246.1.16',
-                                   database='430Group4',
-                                   user='root',
-                                   password='root430group4root')
+    conn = sqlite3.connect('database/430Group4.db')
     cursor = conn.cursor()
-    cursor.execute("select userid from users WHERE username='"+username+"'")
+    cursor.execute("select username from users WHERE username='"+username+"'")
     res = cursor.fetchall()
     if len(res) == 0:
         return 1  # incorrect username, username does not exist
-    userid = res[0][0]
-    cursor.execute("select password from users WHERE userid='"+str(userid)+"'")
+
+    cursor.execute(
+        "select password from users WHERE username='"+str(username)+"'")
     res = cursor.fetchall()
     retPassword = res[0][0]
     if retPassword != password:
         return 2  # incorrect password
-    cursor.execute("SELECT role from users where userid='"+str(userid)+"'")
+    cursor.execute("SELECT role from users where username='"+str(username)+"'")
     res = cursor.fetchall()
     role = res[0][0]
+    cursor.close()
+    conn.close()
     return role
 
 
 def register(name, username, password):
-    conn = mysql.connector.connect(host='35.246.1.16',
-                                   database='430Group4',
-                                   user='root',
-                                   password='root430group4root')
+    conn = sqlite3.connect('database/430Group4.db')
     cursor = conn.cursor()
-    cursor.execute("select userid from users WHERE username='"+username+"'")
+    cursor.execute("select username from users WHERE username='"+username+"'")
     res = cursor.fetchall()
     if len(res) != 0:
         return 1  # username already exists in database
@@ -38,42 +36,41 @@ def register(name, username, password):
             values ('{}','{}','{}','FAN')'''.format(name, username, password)
     cursor.execute(SQL)
     conn.commit()
+    cursor.close()
+    conn.close()
     return 0
 
 
 def getArticle(aid):
-    conn = mysql.connector.connect(host='35.246.1.16',
-                                   database='430Group4',
-                                   user='root',
-                                   password='root430group4root')
+    conn = sqlite3.connect('database/430Group4.db')
     cursor = conn.cursor()
-    cursor.execute("select articletitle, articleheadline, articlebody from article WHERE articleid='"+str(aid)+"'")
+    cursor.execute(
+        "select articletitle, articleheadline, articlebody from article WHERE articleid='"+str(aid)+"'")
     res = cursor.fetchall()
-    return (res[0][0],res[0][1],res[0][2])
+    cursor.close()
+    conn.close()
+    return (res[0][0], res[0][1], res[0][2])
 
-def updateArticle(aid,title,hd,body):
-    conn = mysql.connector.connect(host='35.246.1.16',
-                                   database='430Group4',
-                                   user='root',
-                                   password='root430group4root')
+
+def updateArticle(aid, title, hd, body):
+    conn = sqlite3.connect('database/430Group4.db')
     cursor = conn.cursor()
-    SQL = "UPDATE article SET articletitle ='" +title+"', articleheadline ='" +hd+"', articlebody ='"+body+"' WHERE articleid='"+str(aid)+"'"
+    SQL = "UPDATE article SET articletitle ='" + title+"', articleheadline ='" + \
+        hd+"', articlebody ='"+body+"' WHERE articleid='"+str(aid)+"'"
     cursor.execute(SQL)
     conn.commit()
+    cursor.close()
+    conn.close()
+
 
 def getPlayers(team):
-    conn = mysql.connector.connect(host='35.246.1.16',
-                                   database='430Group4',
-                                   user='root',
-                                   password='root430group4root')
+    conn = sqlite3.connect('database/430Group4.db')
     cursor = conn.cursor()
     cursor.execute("select * from players WHERE team='"+team+"'")
     res = cursor.fetchall()
     players = []
     for row in res:
         players.append(row)
+    conn = sqlite3.connect('database/430Group4.db')
+    cursor = conn.cursor()
     return players
-
-print(getPlayers('womenbb'))
-    
-
